b547c5ecdf87a1c65c5543448d57d2568b871fce
GavryushaWoof <gavgav.cat@yandex.ru>
2020-06-23 15:28:02 +0300

    fifth

--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,28 @@
 import * as invoice from './invoices.json';
 import { Invoice } from './interfaces/Invoice';
 import { Performance } from './interfaces/Performance';
-import { statement, getPlayStorages } from './utils/utils';
+import { getPlayStorages, getAmount, addText, getVolumeCredits, format } from './utils/utils';
+import { Plays } from './interfaces/Plays';
+
+function statement(invoice: Invoice, plays: Plays): string {
+    try {
+        let result = `Счет для ${invoice.customer}\n`;
+        let totalAmount = 0;
+        let volumeCredits = 0;
+        for (let perf of invoice.performance) {
+            const play = plays[perf.playId];
+            let thisAmount = getAmount(play.type, perf.audience);
+            result += addText(play.name, perf.audience, thisAmount);
+            volumeCredits += getVolumeCredits(play.type, perf.audience);
+            totalAmount += thisAmount;
+        }
+        result += `Итого с вас ${format(totalAmount / 100)}\n`;
+        result += `Вы заработали ${volumeCredits} бонусов\n`;
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 const result = statement(invoice as Invoice, getPlayStorages(invoice.performance as Performance[]));
 console.log(result);
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,40 +1,22 @@
-import { Invoice } from "../interfaces/Invoice";
 import { Plays } from "../interfaces/Plays";
 import { PerformanceTypes } from "../enums/PerformanceTypes";
 import { Performance } from '../interfaces/Performance';
 
 export function getPlayStorages(play: Performance[]): Plays {
-    const result = play.reduce((acc: Plays, cur) => { acc[cur.playId] = { name: cur.playId, type: cur.type }; return acc; }, {});
+    const result = play.reduce((acc: Plays, cur) => {
+        acc[cur.playId] = { name: cur.playId, type: cur.type };
+        return acc;
+    }, {});
     return result
 }
 
-export function statement(invoice: Invoice, plays: Plays): string {
-    try {
-        let result = `Счет для ${invoice.customer}\n`;
-        let totalAmount = 0;
-        let volumeCredits = 0;
-        for (let perf of invoice.performance) {
-            const play = plays[perf.playId];
-            let thisAmount = getAmount(play.type, perf.audience);
-            result += addText(play.name, perf.audience, thisAmount);
-            volumeCredits += getVolumeCredits(play.type, perf.audience);
-            totalAmount += thisAmount;
-        }
-        result += `Итого с вас ${format(totalAmount / 100)}\n`;
-        result += `Вы заработали ${volumeCredits} бонусов\n`;
-        return result;
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-const format = new Intl.NumberFormat("ru-RU", {
+export const format = new Intl.NumberFormat("ru-RU", {
     style: "currency",
     currency: "RUB",
     minimumFractionDigits: 2
 }).format;
 
-function getAmount(type: PerformanceTypes, audience: number): number {
+export function getAmount(type: PerformanceTypes, audience: number): number {
     let thisAmount = 0;
     switch (type) {
         case PerformanceTypes.TRAGEDY:
@@ -55,13 +37,13 @@ function getAmount(type: PerformanceTypes, audience: number): number {
     }
 }
 
-function getVolumeCredits(type: PerformanceTypes, audience: number): number {
+export function getVolumeCredits(type: PerformanceTypes, audience: number): number {
     let volumeCredits = Math.max(audience - 30, 0);
     if (PerformanceTypes.COMEDY === type) volumeCredits += Math.floor(audience / 5);
     return volumeCredits
 }
 
-function addText(name: string, audience: number, thisAmount: number): string {
+export function addText(name: string, audience: number, thisAmount: number): string {
     let result = "";
     result += ` ${name}: ${format(thisAmount / 100)}`;
     result += ` (${audience} мест)\n`;
